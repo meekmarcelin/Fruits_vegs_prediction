2b598203b61ea01c8e389f3c4441f72d21dbf660
meekmarcelin@gmail.com <meekmarcelin@gmail.com>
2024-04-17 20:28:33 +0200

    commit

--- models/src/model.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-def load_model(model_path):
-    return tf.keras.models.load_model(model_path)
-
-def predict(model, input_data):
-    prediction = model.predict(input_data)
-    return prediction
